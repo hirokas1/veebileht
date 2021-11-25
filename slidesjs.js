7ab19deed52ea7ceeb66988e6ab6f0d0715017ea
KatariinaIngerma <katariina.ingerma@gmail.com>
2021-11-25 21:10:09 +0200

    Trennid lehekülje javascript ja css täiendused

---
+++ slidesjs.js
@@ -0,0 +1,32 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides"); /* valitakse õige slaid klassi nime järgi, slaididel indeksid ja nende järgi ka punktid.*/
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+/* funktsioon, mis teeb slideshow 
+/* koodi kirjutamisel sain abi lehelt https://www.w3schools.com/howto/howto_js_quotes_slideshow.asp
+*/
